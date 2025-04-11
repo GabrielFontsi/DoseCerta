e3b133313bb5c27c33219d118e5013bbbea9fdfe
Gabriel Fontenele <103197378+GabrielFontsi@users.noreply.github.com>
2025-04-10 23:00:54 -0300

    Setup Inicial (#1)
    * Setup Inicial
    	- Configuração do projeto para desenvolvedor em ViewCode
    
    * Feat: Setup inicial tela login
    
    Criação loginViewController e loginScren
    Exclusão ViewController nativa
    
    * Feat: Ajuste exclusão da login
    
    * feat: Criação da tela LoginViewController e LoginScreen
    
    * feat: Criação da tela de login
    
    ---------
    
    Co-authored-by: Wellington <wellingtonmendes.silva@yahoo.com>

---
+++ DoseCerta/DoseCerta/LoginViewController/LoginScreen.swift
@@ -0,0 +1,20 @@
+//
+//  LoginScreen.swift
+//  DoseCerta
+//
+//  Created by Wellington  on 02/04/25.
+//
+
+import UIKit
+
+class LoginScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DoseCerta/DoseCerta/UIComponents/CustomTextField.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTextField.swift
+//  DoseCerta
+//
+//  Created by Wellington  on 05/04/25.
+//
+
+import UIKit
+
+class CustomTextField: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
