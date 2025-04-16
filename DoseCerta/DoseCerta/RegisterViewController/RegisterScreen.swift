f48244b66b1c14d20bc2617c334416c6c8b29c7b
Wellington <wellingtonmendes.silva@yahoo.com>
2025-04-15 22:25:58 -0300

    feat: Criação da tela RegisterViewController e RegisterScreen

---
+++ DoseCerta/DoseCerta/RegisterViewController/RegisterScreen.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterScreen.swift
+//  DoseCerta
+//
+//  Created by Wellington  on 12/04/25.
+//
+
+import UIKit
+
+class RegisterScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DoseCerta/DoseCerta/RegisterViewController/RegisterView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterView.swift
+//  DoseCerta
+//
+//  Created by Wellington  on 12/04/25.
+//
+
+import UIKit
+
+class RegisterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
